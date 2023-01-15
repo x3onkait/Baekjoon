d068260f97f5bcfa3f463725ec60e807c14e509f
luexr <47494005+x3onkait@users.noreply.github.com>
2023-01-15 20:48:52 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+    int first, second;
+    scanf("%d %d", &first, &second);
+
+    printf("%d\n", first + second);
+    printf("%d\n", first - second);
+    printf("%d\n", first * second);
+    printf("%d\n", first / second);
+    printf("%d\n", first % second);
+
+    return 0;
+
+}
